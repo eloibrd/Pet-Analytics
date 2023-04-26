16449b20457da624c61380bc6fb410b821ee8e31
Eloi BERNARD <eloi0bernard@gmail.com>
2023-04-26 15:44:26 +0200

    tweak main to run uvicorn & provide a config

---
+++ app/core/config.py
@@ -0,0 +1,51 @@
+import os
+
+from pydantic import BaseSettings
+
+
+class Config(BaseSettings):
+    """Base application configuration to overwrite."""
+
+    ENV: str = "development"
+    DEBUG: bool = True
+    APP_HOST: str = "0.0.0.0"
+    APP_PORT: int = 8000
+
+
+class LocalConfig(Config):
+    """Local development configuration.
+    Overwrites `BaseConfig`.
+    """
+
+    pass
+
+
+class ProductionConfig(Config):
+    """Production configuration.
+    Overwrites `BaseConfig`.
+    """
+
+    DEBUG: bool = False
+
+
+def get_config() -> Config:
+    """Provides the `Config` setup matching current environment variable "ENV".
+
+    Returns:
+        Config: The configuration to use.
+    """
+
+    # Set to "local" by default if the environment variable is not set.
+    env = os.getenv("ENV", "local")
+    # List all configurations available.
+    config_type = {
+        "local": LocalConfig(),
+        "prod": ProductionConfig(),
+    }
+    # Return the config matching the current ENV.
+    return config_type[env]
+
+
+config: Config = get_config()
+"""The current app configuration.
+"""
